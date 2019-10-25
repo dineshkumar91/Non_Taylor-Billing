6e342688a4d3708e73cd29ac3af85dc1753079ce
dineshkumar91 <dineshkumar.sundararajan@parker.com>
2019-10-25 14:17:58 -0500

    Adding config file

---
+++ config.py
@@ -0,0 +1,65 @@
+username = "BillingAdmin@parker.com"
+password = "7yf9tn7c6oj45c2ehv9ou"
+
+partnumber = {'164632': 36,
+              '164633': 24,
+              '164630': 3,
+              '164631': 6,
+              '166035': 36,
+              '165340': 12,
+              '166033': 12,
+              '166032': 12}
+
+
+billing_orgID = 'a09ad270-3b21-11e7-893e-f94b4bc7246b'
+
+bill_org = {'7a064320-4d85-11e8-938c-275970980c0c':'AM-DYN-IC',
+            '17ad9f98-33df-48a0-8c4f-99468e22e129':'Barko Hydraulics HQ',
+            '5813dd40-5951-11e8-9266-ffd81a3f934a':'Controlled Motion Solutions',
+            '8d0fbae0-7301-11e8-afcd-8119891950e5':'Feldcamp Equipment',
+            '410866d0-36aa-11e8-8e2e-6f1fbe0d469d':'Godwin Manufacturing',
+            '7ff34c90-36b2-11e8-8e2e-6f1fbe0d469d':'GS Global Resources',
+            'f1625643-da6e-4537-828d-0931301f8add':'Hagie Manufacturing',
+            '5e749d50-4d85-11e8-a6a0-cf0b8a4d1908':'Hydradyne LLC',
+            'c8c46ab9-7e15-4efd-94ea-433d72a35436':'Hyspeco',
+            '1c6702d1-397d-4b76-b9b8-6290293d3f6d':'JH Fletcher & CO',
+            'fa97ce00-7099-11e8-afcd-8119891950e5':'JLG Demo',
+            'f3f8a230-5551-11e8-abce-8dc1d92ca601':'Manitex Inc',
+            '336aaf03-c903-4842-a978-b47ebaff4ae9':'Michigan Fluid Power',
+            'b6a7a266-e6c3-43d6-b6f8-821718565f4b':'MFCP Inc',
+            'd43262eb-469f-4a8b-a66c-0e0de52769b8':'Parker Australia',
+            'b0afa22c-f8a3-4ccd-a6ad-8d59cca53081':'Parker Canada',
+            'bc76bfd0-9981-11e8-8f96-5bc810174da8':'Parker EMEA',
+            'd447ca6b-75f2-4d67-8e84-58dff4a723bf':'Pettibone-Traverse Lift LLC',
+            '973e4b99-9fde-42aa-b250-3c3bfe8f0728':'RG Group',
+            'e6473df0-4704-11e8-a8fe-4b7939bceb9f':'Tymco Inc.',
+            'cfd2b669-787f-481c-a57f-5561e70d8211':'Workhorse',
+            '612152bc-c7d8-46f2-aa52-037ef9a6e2ea':'Miller Technology Incorporated'}
+
+
+
+cust_no = {'AM-DYN-IC':'12207',
+           'Barko Hydraulics HQ':'12257',
+           'Controlled Motion Solutions':'12269',
+           'Feldcamp Equipment':'12212',
+           'Godwin Manufacturing':'13089',
+           'GS Global Resources':'12214',
+           'Hagie Manufacturing':'12215',
+           'Hydradyne LLC':'12217',
+           'Hyspeco':'12287',
+           'JH Fletcher & CO':'13201',
+           'JLG Demo':'12495',
+           'Manitex Inc':'13241',
+           'Michigan Fluid Power':'12297',
+           'MFCP Inc':'12296',
+           'Parker Australia':'12310',
+           'Parker Canada':'12155',
+           'Parker EMEA':'12110',
+           'Pettibone-Traverse Lift LLC':'13001',
+           'RG Group':'12323',
+           'Tymco Inc.':'11610',
+           'Workhorse':'13066',
+           'Miller Technology Incorporated':'Miller Technology Incorporated'}
+
+custom_months = {'Barko Hydraulics HQ':'12'}
+
